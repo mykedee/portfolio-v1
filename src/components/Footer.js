f46f4f24975b32b0e5c349727979119f89cc18c2
mykedee <52767074+mykedee@users.noreply.github.com>
2024-02-03 09:27:13 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,8 @@ import React from "react";
 const Footer = (props) => {
 	return (
 		<>
-			{/* <div
-				className={`flex justify-center items-center py-5 ${
-					props.showDarkMode ? "dark" : "bg-dash-bg"
-				} `}
-			> */}
+			
 			<div className="flex justify-center items-center py-5">
-				{/* <div className="bg-black text-wheat flex justify-center items-center py-5"> */}
-
 				<div>
 					<p className="font-light text-sm">
 						&copy; {new Date().getFullYear()}
