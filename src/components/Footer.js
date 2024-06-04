da15a2eb8b93c778e8c289d94e979572775bfd5a
mykedee <mikededeji@gmail.com>
2024-06-04 07:19:42 +0100

    recent changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,16 @@
 import React from "react";
 
 const Footer = (props) => {
-<<<<<<< HEAD
   return (
     <>
-      <div className="dark:bg-red h-auto py-5">
-        <div className="flex justify-center items-center pt-5 font-body">
-          <div>
-            <p className="font-light text-sm ">
-              &copy; {new Date().getFullYear()}
-            </p>
-          </div>
+      <div className="flex justify-center items-center py-5">
+        <div>
+          <p className="font-light text-sm">
+            &copy; {new Date().getFullYear()}
+          </p>
         </div>
       </div>
     </>
   );
-=======
-	return (
-		<>
-			
-			<div className="flex justify-center items-center py-5">
-				<div>
-					<p className="font-light text-sm">
-						&copy; {new Date().getFullYear()}
-					</p>
-				</div>
-			</div>
-		</>
-	);
->>>>>>> 2a0d7ca7b43caa28e796d76b08ff7e27027e624f
 };
 export default Footer;
