ad1eaf2964358b61bb1b241c32f6386d9fbaa9e0
mykedee <mikededeji@gmail.com>
2024-07-22 12:08:48 +0100

    latest update

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -5,9 +5,7 @@ import Projects from "../components/Projects";
 
 const HomeScreen = () => {
   return (
-    <div
-      className="sticky lg:overflow-x-hidden overflow-x-auto pt-[5%] pb-[1%] min-h-[100vh]"
-    >
+    <div className="sticky lg:overflow-x-hidden overflow-x-auto pt-[5%] pb-[1%] min-h-[100vh]">
       <div className="my-12 lg:my-auto w-full mx-auto">
         <div className="flex md:flex-row justify-center items-center flex-col">
           <div className="w-11/12 md:w-4/6 mx-auto my-1 ">
@@ -16,8 +14,8 @@ const HomeScreen = () => {
             </h1>
             <p className="font-body text-left w-full lg:w-3/5 mx-auto md:text-center text-base font-light my-2">
               I am a Frontend Developer skilled at creating visually pleasing,
-              high-performance, and scalable web applications using modern web
-              technologies.
+              robust, and efficient web applications optimized for scalability
+              and performance, utilizing advanced web technologies.
             </p>
             <p className="font-body block text-left text-base w-full lg:w-3/5 mx-auto md:text-center font-light my-2">
               Tools and technologies I work with include
@@ -70,7 +68,7 @@ const HomeScreen = () => {
           <h1 className=" bg-gradient-to-r from-[#020024] via-[#241401] to-[#4c0c00] inline-block text-transparent bg-clip-text dark:bg-gradient-to-r dark:from-[#871701] dark:via-[#b87c39] dark:to-[#771300]  text-center font-bold text-3xl">
             Stuff I have built...
           </h1>
-          <Projects/>
+          <Projects />
         </div>
       </div>
     </div>
